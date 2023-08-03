0d11b1ce7c8fc9dff709acfe24507765a3a64942
Jananiksha <jananiksha664@gmail.com>
2023-08-03 14:59:33 +0530

    Created Successfully

--- JS/sample.js
+++ JS/sample.js
@@ -1,4 +1,4 @@
-var a= 120;
+/* var a= 120;
 console.log(a)
 let b=120
 console.log("This is the value declared in var type:"+" "+b)
@@ -11,3 +11,83 @@ console.log("This is the value declared in let type:"+" "+ab)
 const c= 20
 console.log("This is the value declared in const type:"+" "+c)
 
+//if..else
+
+a = 0
+if (a===0){
+    a=a+1
+    console.log(a)
+}
+else if(a>0){
+    console.log("The number is positive")
+}
+else{
+    console.log("The number is negative")
+}
+
+i=0
+while(i<=a){
+    console.log(i)
+    i++
+}
+array = [10,20]//one liner function arrow op
+array.forEach(element =>  console.log(element))//arrow op will fetch the elements
+ */
+const array =["Jan",23,2,10.00,Date()]
+for(var ele in array){
+    console.log(array[ele])
+}
+console.log("after change")
+array.push("3rd years")
+
+for (var ele in array){
+    console.log(array[ele])
+}
+
+console.log("After poping")
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+array.pop()
+console.log(array)
+for (var ele in array){
+    console.log(array[ele])
+}
+
+if(array.length>0){
+    console.log("elements are present")
+    for (var ele in array){
+        console.log(array[ele])
+
+}}
+else{
+    console.log("No Elements")
+}
+
+array.push(1)
+console.log(array)
+
+
+//objects
+a={
+    car:"Audi",
+    model:"AB",
+    airbags: 8,
+    wheels: 5,
+    color:"blue"
+}
+
+console.log(a['color'])
+console.log(a.airbags)
+var some='model'
+console.log(a[some])
+console.log(a)
+a.car= "BMW"
+console.log(a)
+a.sunroof = {isPresent: "Yes", Shape: "Rectangle"}
+console.log(a.sunroof .isPresent) 
+for (var ele in a){
+    console.log(ele +":"+ a[ele])
+}
