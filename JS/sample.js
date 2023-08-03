9d6ace1e64949370f9a09b4870147d6326bdcaf9
Jananiksha <jananiksha664@gmail.com>
2023-08-03 11:31:00 +0530

    Created index.html

--- JS/sample.js
+++ JS/sample.js
@@ -1,2 +1,13 @@
 var a= 120;
 console.log(a)
+let b=120
+console.log("This is the value declared in var type:"+" "+b)
+{
+console.log("This is the value declared in var type before altering: "+" "+a )
+}
+console.log("This is the value declared in var type after altering: "+" "+a )
+let ab=10
+console.log("This is the value declared in let type:"+" "+ab)
+const c= 20
+console.log("This is the value declared in const type:"+" "+c)
+
